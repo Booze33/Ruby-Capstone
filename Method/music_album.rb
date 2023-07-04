c78bf42a1211ebb5e6122ca5ff3c40f9ad6712b5
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-04 16:14:55 -0500

    set MusicAlbum constructor

--- Method/music_album.rb
+++ Method/music_album.rb
@@ -1,5 +1,8 @@
 require_relative '../item'
 
 class MusicAlbum < Item #  class properties: on_spotify:boolean
-
+  def initialize(publish_date, on_spotify)
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
 end
