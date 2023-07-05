32647d7ee246fe690d2d65f5733ca1f8c21a71e4
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-05 12:50:47 -0500

    fix cab_be_archived? method on music_album method

--- Method/music_album.rb
+++ Method/music_album.rb
@@ -3,19 +3,20 @@ require_relative '../item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(genre, author, source, label, publish_date, on_spotify) # rubocop:disable Metrics/ParameterLists
-    super(genre, author, source, label, publish_date)
+  def initialize(publish_date, on_spotify) # rubocop:disable Metrics/ParameterLists
+    super(publish_date)
     @on_spotify = on_spotify
   end
 
   def can_be_archived?
-    super and @on_spotify
+    if super and @on_spotify
+      @archived = true
+    end
   end
 end
 
-# album = MusicAlbum.new('Metal', 'Acdc', 'From a friend', 'Gift', '2010-01-30', true)
-
-# album.can_be_archived?
-# album.move_to_archive
-
-# p album
+# music = MusicAlbum.new('2010-01-30', true)
+# genre = Genre.new('Comedy')
+# music.genre = genre
+# music.move_to_archive
+# p music
