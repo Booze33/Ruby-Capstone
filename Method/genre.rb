2ea37509ead1bb147fb3a71e0d18316707550a33
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-04 23:53:29 -0500

    create Genre class

---
+++ Method/genre.rb
@@ -0,0 +1,11 @@
+require_relative '../item'
+
+class Genre
+  attr_accessor :id, :name, :items
+
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+end
