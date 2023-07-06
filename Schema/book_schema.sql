f12ce5bfe290174f0e03385d78083bb6d765574b
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-06 14:34:19 -0500

    add music_albulm and genre tables on schema file

--- Schema/book_schema.sql
+++ Schema/book_schema.sql
@@ -20,3 +20,21 @@ CREATE TABLE books_labels (
   FOREIGN KEY (book_id) REFERENCES books(id),
   FOREIGN KEY (label_id) REFERENCES labels(id)
 );
+
+CREATE TABLE genre (
+  id INT PRIMARY KEY,
+  name varchar(50)
+);
+
+CREATE TABLE music_album (
+  id INT PRIMARY KEY,
+  genre varchar(50),
+  author_name varchar(50),
+  author_lastname varchar(50),
+  source_name varchar(50),
+  label_title varchar(50),
+  label_color varchar(50),
+  pushished_date DATE,
+  archived boolean,
+  on_spotity boolean
+)
