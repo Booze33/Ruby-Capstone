f3b36a19020cf1072cca4736e59483f85cb60526
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-07 09:34:37 -0500

    update schema

--- Schema/schema.sql
+++ Schema/schema.sql
@@ -1,33 +1,51 @@
-CREATE TABLE books (
-  id INT PRIMARY KEY,
-  title TEXT,
-  publish_date DATE,
-  publisher TEXT,
-  color TEXT,
-  cover_state INT,
-  archived INT
+CREATE TABLE authors(
+  id INT, 
+  first_name VARCHAR(255), 
+  last_name VARCHAR(255), 
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE labels (
-  id INT PRIMARY KEY,
-  title TEXT,
-  color TEXT
+CREATE TABLE labels(
+  id INT, 
+  title VARCHAR(255), 
+  color VARCHAR(255), 
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE books_labels (
-  book_id INT,
-  label_id INT,
-  FOREIGN KEY (book_id) REFERENCES books(id),
-  FOREIGN KEY (label_id) REFERENCES labels(id)
+CREATE TABLE genres(
+  id INT, 
+  name VARCHAR(255), 
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE genre (
-  id INT PRIMARY KEY,
-  name varchar(50)
+CREATE TABLE books(
+    id INT, 
+    name VARCHAR(255), 
+    published_date DATE, 
+    publisher VARCHAR(255), 
+    cover_state VARCHAR(255), 
+    archived BOOLEAN, 
+    author_id INT REFERENCES authors(id), 
+    label_id INT REFERENCES labels(id), 
+    genre_id INT REFERENCES genres(id), 
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE games(
+  id INT, 
+  name VARCHAR(255), 
+  published_date DATE, 
+  last_played_date DATE, 
+  multiplayer BOOLEAN, 
+  archived BOOLEAN, 
+  author_id INT REFERENCES authors(id), 
+  label_id INT REFERENCES labels(id), 
+  genre_id INT REFERENCES genres(id), 
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE musicalbums(
-  id INT PRIMARY KEY, 
+  id INT, 
   name VARCHAR(255), 
   published_date DATE, 
   on_spotify BOOLEAN, 
@@ -35,6 +53,7 @@ CREATE TABLE musicalbums(
   author_id INT REFERENCES authors(id), 
   label_id INT REFERENCES labels(id), 
   genre_id INT REFERENCES genres(id), 
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE items (
