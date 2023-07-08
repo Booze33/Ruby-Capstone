59aa0ec92a12aabc47dfd826f96836cfc2a4eab7
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-07 19:52:08 -0500

    list genres, authors and labels

--- app.rb
+++ app.rb
@@ -14,32 +14,8 @@ class App
     @game_options = GameOptions.new(@games)
   end
 
-  def load_music_albums
-    @albums.load_music_albums
-    @main.display_menu
-  end
-
   def list_books
     @book_options.list_books(@books)
-    @main.display_menu
-  end
-
-  def list_labels
-    if @albums.empty? and @books.empty?
-      puts 'No genres added yet'
-    else
-      puts 'Listing all labels'
-      @albums.each do |label|
-        puts "Name: #{label.label.title}"
-      end
-      @books.each do |label|
-        puts "Name: #{label.label.title}"
-      end
-    end
-  end
-
-  def add_book
-    @book_options.add_book
     @main.display_menu
   end
 
@@ -48,15 +24,66 @@ class App
     @main.display_menu
   end
 
+  def list_games
+    @game_options.list_games(@games)
+    @main.display_menu
+  end
+
   def list_genres
-    if @albums.empty?
+    if @albums.empty? and @books.empty? and @games.empty?
       puts 'No genres added yet'
     else
       puts 'Listing all genres'
       @albums.each do |genre|
         puts "Name: #{genre.genre.name}"
       end
+      @books.each do |genre|
+        puts "Name: #{genre.genre.name}"
+      end
+      @games.each do |genre|
+        puts "Name: #{genre.genre.name}"
+      end
     end
+    @main.display_menu
+  end
+
+  def list_labels
+    if @albums.empty? and @books.empty? and @games.empty?
+      puts 'No genres added yet'
+    else
+      puts 'Listing all labels'
+      @albums.each do |label|
+        puts "Name: #{label.label.title} Color: #{label.label.color}"
+      end
+      @books.each do |label|
+        puts "Name: #{label.label.title} Color: #{label.label.color}"
+      end
+      @games.each do |label|
+        puts "Name: #{label.label.title} Color: #{label.label.color}"
+      end
+    end
+  end
+  
+  def list_authors
+    if @albums.empty? and @books.empty? and @games.empty?
+      puts 'No authors added yet'
+    else
+      puts 'Listing all authors'
+      @albums.each do |author|
+        puts "Name: #{author.author.first_name} #{author.author.last_name}"
+      end
+      @books.each do |author|
+        puts "Name: #{author.author.first_name} #{author.author.last_name}"
+      end
+      @games.each do |author|
+        puts "Name: #{author.author.first_name} #{author.author.last_name}"
+      end
+    end
+    @main.display_menu
+  end
+
+  def add_book
+    @book_options.add_book
     @main.display_menu
   end
 
@@ -65,33 +92,18 @@ class App
     @main.display_menu
   end
 
-  def quit
-    puts 'Bye!'
-    p @albums
-    p @books
-    p @games
-    exit
-  end
-
   def add_game
     @game_options.add_game
     @main.display_menu
   end
 
-  def list_games
-    @game_options.list_games(@games)
-    @main.display_menu
+  def quit
+    puts 'Bye!'
+    exit
   end
 
-  def list_authors
-    if @albums.empty?
-      puts 'No authors added yet'
-    else
-      puts 'Listing all authors'
-      @albums.each do |author|
-        puts "Name: #{author.author.first_name} #{author.author.last_name}"
-      end
-    end
+  def load_data
+    @albums.load_music_albums
     @main.display_menu
   end
 end
